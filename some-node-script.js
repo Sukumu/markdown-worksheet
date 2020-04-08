4bbdc209d5798d517286b58c2d3cc2f7fc9924b7
Sukumu <48925529+Sukumu@users.noreply.github.com>
2020-04-08 13:22:57 -0500

    Create some-node-script.js

---
+++ some-node-script.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack'); //to access webpack runtime
+const configuration = require('./webpack.config.js');
+
+let compiler = webpack(configuration);
+
+new webpack.ProgressPlugin().apply(compiler);
+
+compiler.run(function(err, stats) {
+  // ...
+});
